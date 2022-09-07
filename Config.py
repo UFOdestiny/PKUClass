46e00d16cc80b5e6e1211dd69ea126eef994e122
UFOdestiny <1976438440@qq.com>
2022-09-07 22:08:12 +0800

    ver 1.3 simpleton

--- Config.py
+++ Config.py
@@ -1,26 +1,39 @@
 class User:
-    username = "123"
-    password = "123"
+    username = "1312314"
+    password = "31"
 
 
 class User2:
-    username = "123"
-    password = "123"
+    username = "1312310"
+    password = "2313"
 
 
 class User3:
-    username = "123"
-    password = "123"
+    username = "13131231"
+    password = "231"
+
+
+class User4:
+    username = "12"
+    password = "132L"
 
 
 class TuJianSetting:
-    uname = '123'
-    pwd = '123'
+    uname = '12sy'
+    pwd = '12120'
 
 
 class Email:
-    _user = "123@qq.com"
-    _code = "123"
+    _user = "1120@qq.com"
+    _code = "1212i"
 
-    _sender = "123@qq.com"
-    _receivers = ["123@qq.com", ]
+    _sender = "120@qq.com"
+    _receivers = ["12@qq.com"]
+
+
+class LogSetting:
+    path = "log"
+
+
+class CaptchaSetting:
+    path = "captcha"
